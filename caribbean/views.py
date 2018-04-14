1ea896b0af6093c88f747a7b5a1c34704c93a269
amaurs <amaury.gtz@gmail.com>
2018-04-14 14:52:13 -0500

    Added authentication handle to map.

--- caribbean/views.py
+++ caribbean/views.py
@@ -4,13 +4,19 @@ from rest_framework import viewsets
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
+
 class SampleViewSet(viewsets.ModelViewSet):   
     queryset = MadmexCaribesample.objects.all()
     serializer_class = SampleSerializer
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return FooModel.objects.all()
+        return FooModel.objects.filter(owner=self.request.user)
 
 class TagViewSet(viewsets.ModelViewSet):   
     queryset = MadmexTag.objects.all()
     serializer_class = TagSerializer
+
 
 class CountryViewSet(viewsets.ModelViewSet):   
     queryset = MadmexCountry.objects.all()
