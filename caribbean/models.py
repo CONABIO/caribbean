3c0bbcbe2702655d2b0c76483dd384ad1f9c47d6
amaurs <amaury.gtz@gmail.com>
2018-04-14 12:53:44 -0500

    Added models for database.

---
+++ caribbean/models.py
@@ -0,0 +1,43 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.contrib.gis.db import models
+
+
+class MadmexCaribesample(models.Model):
+    the_geom = models.GeometryField()
+    added = models.DateTimeField()
+    confidence = models.FloatField()
+    st_area = models.FloatField()
+    country = models.ForeignKey('MadmexCountry', models.DO_NOTHING)
+    tag = models.ForeignKey('MadmexTag', models.DO_NOTHING)
+    tag_editable = models.ForeignKey('MadmexTag', models.DO_NOTHING, related_name='tag_editable_interpreter')
+
+    class Meta:
+        managed = False
+        db_table = 'madmex_caribesample'
+
+class MadmexCountry(models.Model):
+    name = models.CharField(unique=True, max_length=100)
+    the_geom = models.MultiPolygonField()
+    added = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'madmex_country'
+
+class MadmexTag(models.Model):
+    scheme = models.CharField(max_length=50)
+    value = models.CharField(max_length=150)
+    numeric_code = models.IntegerField()
+    color = models.CharField(max_length=7)
+
+    class Meta:
+        managed = False
+        db_table = 'madmex_tag'
+
+
